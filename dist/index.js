3a8291c6eea380e405a30adb4930e0c683ab2d54
Jack.Wang <jack.wang@integem.com>
2019-12-09 19:39:41 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -358,14 +358,13 @@ module.exports = /******/ (function(modules, runtime) {
       async function run() {
         try {
           const packagePath = core.getInput('path', { required: true })
+          const key = core.getInput('key', { required: true })
 
           const package_file = fs.readFileSync(packagePath)
-
           const package = JSON.parse(package_file)
-
           console.log(JSON.stringify(package, null, 2))
 
-          core.setOutput('package', package)
+          core.setOutput(key, package[key])
         } catch (error) {
           core.setFailed(error.message)
         }
